2f3a6fbff196922a1c59d476947c1c56eec7d5c7
Mark Cavage <mcavage@gmail.com>
2012-04-16 09:47:02 -0700

    Initial test harness (add only)

--- test/add.test.js
+++ test/add.test.js
@@ -1,13 +1,12 @@
 // Copyright 2012 Joyent, Inc.  All rights reserved.
 //
-// You can set PG_URL to connect to a database, and LOG_LEVEL to turn on
+// You can set UFDS_URL to connect to a server, and LOG_LEVEL to turn on
 // bunyan debug logs.
 //
 
-var assert = require('assert');
-var util = require('util');
+var sprintf = require('util').format;
+var uuid = require('node-uuid');
 
-var Logger = require('bunyan');
 if (require.cache[__dirname + '/helper.js'])
     delete require.cache[__dirname + '/helper.js'];
 var helper = require('./helper.js');
@@ -16,12 +15,123 @@ var helper = require('./helper.js');
 
 ///--- Globals
 
+var CLIENT;
+var SUFFIX = process.env.UFDS_SUFFIX || 'o=smartdc';
+var DN_FMT = 'uuid=%s, ' + SUFFIX;
+
 var test = helper.test;
 
 
 
 ///--- Tests
 
-test('empty', function (t) {
-    t.done();
+test('setup', function (t) {
+    helper.createClient(function (err, client) {
+        t.ifError(err);
+        t.ok(client);
+        CLIENT = client;
+        t.done();
+    });
+});
+
+
+test('no suffix', function (t) {
+    var entry = {
+        o: uuid(),
+        objectclass: 'organization'
+    };
+    CLIENT.add('o=' + uuid(), entry, function (err) {
+        t.ok(err);
+        t.equal(err.name, 'NoSuchObjectError');
+        t.done();
+    });
+});
+
+
+test('add suffix', function (t) {
+    var entry = {
+        o: 'smartdc',
+        objectclass: 'organization'
+    };
+    CLIENT.add(SUFFIX, entry, function (err) {
+        t.ifError(err);
+        t.done();
+    });
+});
+
+
+test('add child missing parent', function(t) {
+    var entry = {
+        cn: 'unit',
+        objectClass: 'organization',
+        o: 'test'
+    };
+    CLIENT.add('cn=fail, ou=fail, ' + SUFFIX, entry, function(err) {
+        t.ok(err);
+        t.equal(err.name, 'NoSuchObjectError');
+        t.done();
+  });
+});
+
+
+test('add child', function (t) {
+    var dn = sprintf(DN_FMT, 'unit-test');
+    var entry = {
+        login: 'unit_test',
+        email: 'unit_test@joyent.com',
+        uuid: uuid(),
+        userpassword: 'secret',
+        objectclass: 'sdcperson'
+    };
+    CLIENT.add(dn, entry, function (err) {
+        t.ifError(err);
+        t.done();
+    });
+});
+
+
+test('add child already exists', function (t) {
+    var dn = sprintf(DN_FMT, 'unit-test');
+    var entry = {
+        login: 'foo',
+        email: 'foo@joyent.com',
+        uuid: uuid(),
+        userpassword: 'secret',
+        objectclass: 'sdcperson'
+    };
+    CLIENT.add(dn, entry, function (err) {
+        t.ok(err);
+        t.equal(err.name, 'EntryAlreadyExistsError');
+        t.done();
+    });
+});
+
+
+test('add child unique conflict', function (t) {
+    var id = uuid();
+    var dn = sprintf(DN_FMT, id);
+
+    var entry = {
+        login: 'a' + id.substr(0, 7),
+        email: 'unit_test@joyent.com',
+        uuid: id,
+        userpassword: 'secret',
+        objectclass: 'sdcperson'
+    };
+    CLIENT.add(dn, entry, function (err) {
+        t.ok(err);
+        t.equal(err.name, 'ConstraintViolationError');
+        t.done();
+    });
+});
+
+
+test('teardown', function (t) {
+    helper.cleanup(SUFFIX, function (err) {
+        t.ifError(err);
+        CLIENT.unbind(function (err2) {
+            t.ifError(err2);
+            t.done();
+        });
+    });
 });
--- test/helper.js
+++ test/helper.js
@@ -4,9 +4,12 @@
 // a common logger for all tests.
 //
 
+var assert = require('assert');
 var fs = require('fs');
 
 var Logger = require('bunyan');
+var ldapjs = require('ldapjs');
+var moray = require('moray-client');
 
 
 
@@ -37,6 +40,75 @@ module.exports = {
 
     test: function test(name, tester) {
         module.parent.exports[name] = tester;
+    },
+
+    createClient: function createClient(nobind, callback) {
+        if (typeof (nobind) === 'function') {
+            callback = nobind;
+            nobind = false;
+        }
+        assert.equal(typeof (callback), 'function');
+
+        var client = ldapjs.createClient({
+            connectTimeout: 1000,
+            log: LOG,
+            url: (process.env.UFDS_URL || 'ldap://localhost:1389')
+        });
+
+        client.once('error', function (err) {
+            return callback(err);
+        });
+
+        client.once('connect', function () {
+            if (nobind)
+                return callback(null, client);
+
+            var dn = process.env.UFDS_BIND_DN || 'cn=root';
+            var pw = process.env.UFDS_BIND_PW || 'secret';
+            client.bind(dn, pw, function (err) {
+                if (err)
+                    return callback(err);
+
+                return callback(null, client);
+            });
+        });
+    },
+
+    cleanup: function cleanupMoray(suffix, callback) {
+        var client = moray.createClient({
+            url: process.env.MORAY_URL || 'http://localhost:8080',
+            log: LOG.child({
+                component: 'moray'
+            }),
+            retry: false,
+            connectTimeout: 1000
+        });
+
+        var bucket = process.env.MORAY_BUCKET ||
+            'ufds_' + suffix.replace('=', '_');
+
+        var keys = [];
+        var req = client.keys(bucket, {limit: 1000});
+        req.on('error', function (err) {
+            return callback(err);
+        });
+        req.on('keys', function (_keys) {
+            if (req.hasMoreKeys())
+                req.next();
+            Object.keys(_keys).forEach(function (k) {
+                keys.push(k);
+            });
+        });
+        req.on('end', function () {
+            var finished = 0;
+            keys.forEach(function (k) {
+                client.del(bucket, k, function (err) {
+                    assert.ifError(err);
+                    if (++finished === keys.length)
+                        return callback(null);
+                });
+            });
+        });
     }
 };
 

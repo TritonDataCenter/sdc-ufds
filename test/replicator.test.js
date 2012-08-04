3729485336e098c6f64982e3d70ab1b32379b5c9
Andres Rodriguez <andres@joyent.com>
2012-08-03 20:30:49 -0500

    CAPI-138: Working test with adds/deletes. Need to find a way to detect what's the last changenumber in the remote changelog

--- test/replicator.test.js
+++ test/replicator.test.js
@@ -7,6 +7,7 @@
 
 var assert = require('assert-plus');
 
+var fixtures = require('./fixtures');
 var Replicator = require('../lib/index');
 var REPLICATOR;
 
@@ -20,6 +21,7 @@ var LOG = bunyan.createLogger({
 });
 
 var CUSTOMER_DN = 'uuid=930896af-bf8c-48d4-885c-6573a94b1853, ou=users, o=smartdc';
+var REPLICATION_QUERY = '/ou=users,%20o=smartdc??sub?(&(!(objectclass=vm))(!(login=admin)))';
 
 var LOCAL_UFDS = {
 	url: 'ldap://' + (process.env.LOCAL_UFDS_IP || '127.0.0.1:1389'),
@@ -28,12 +30,14 @@ var LOCAL_UFDS = {
 	bindCredentials: 'secret'
 };
 
+
 var REMOTE_UFDS = {
-	url: 'ldaps://' + (process.env.UFDS_IP || '10.99.99.14/ou=users,%20o=smartdc??sub?(!(objectclass=vm))'),
+	url: 'ldaps://' + (process.env.UFDS_IP || '10.99.99.14' + REPLICATION_QUERY),
 	maxConnections: 1,
 	bindDN: 'cn=root',
 	bindCredentials: 'secret'
 };
+
 
 var REPLICATOR_OPTS = {
 	log: LOG,
@@ -41,6 +45,7 @@ var REPLICATOR_OPTS = {
 	remoteUfds: REMOTE_UFDS,
 	checkpointDn: 'cn=replicator, datacenter=coal, o=smartdc'
 };
+
 
 exports.initReplicator = function(t) {
 	REPLICATOR = new Replicator(REPLICATOR_OPTS);
@@ -51,10 +56,56 @@ exports.initReplicator = function(t) {
 	});
 };
 
-exports.step = function(t) {
+
+exports.bootstrap = function(t) {
+	var remote = REPLICATOR.remoteUfds;
+
+	var user = fixtures.user;
+	var key = fixtures.key;
+
+	remote.add(user.dn, user.object, function (errA, resA) {
+		assert.ifError(errA);
+
+		remote.add(key.dn, key.object, function (errB, resB) {
+			assert.ifError(errB);
+
+			t.done();
+		});
+	});
+};
+
+
+// Give the replicator time to catch up
+exports.bootstrapCatchUp = function(t) {
 	setTimeout(function () {
     	t.done();
-	}, 2000);
+	}, 180000);
+};
+
+
+// Create delete operations and let them replicate
+exports.unbootstrap = function(t) {
+	var remote = REPLICATOR.remoteUfds;
+
+	var userDn = fixtures.user.dn;
+	var keyDn = fixtures.key.dn;
+
+	remote.del(keyDn, function (errA, resA) {
+		assert.ifError(errA);
+
+		remote.del(userDn, function (errB, resB) {
+			assert.ifError(errB);
+
+			t.done();
+		});
+	});
+};
+
+
+exports.unbootstrapCatchUp = function(t) {
+	setTimeout(function () {
+    	t.done();
+	}, 3000);
 };
 
 
@@ -63,13 +114,8 @@ exports.cleanup = function(t) {
         t.done();
     });
 
-    // Cleanup the test replication
-    REPLICATOR.localUfds.del(CUSTOMER_DN, function (err, res) {
+	REPLICATOR.checkpoint.set(0, function(err) {
 		assert.ifError(err);
-
-    	REPLICATOR.checkpoint.set(0, function(err) {
-			assert.ifError(err);
-    		REPLICATOR.stop();
-    	});
-    });
+		REPLICATOR.stop();
+	});
 };

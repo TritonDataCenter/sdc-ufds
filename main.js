163ed68783f90b40b76d9545bbb917fa427cda8b
Pedro P. Candel <pedro@joyent.com>
2012-09-27 16:51:22 +0200

    CAPI-163 and all the issues found while rewriting (CAPI-164, CAPI-166).
    CAPI-163: Added utility to change log levels using SIGUSR1/SIGUSR2 POSIX signals.
    CAPI-164: Removed config.trees and the associated code not needed.
    CAPI-166: Call moray connection callback only once.

--- main.js
+++ main.js
@@ -6,21 +6,11 @@ var fs = require('fs');
 var path = require('path');
 
 var Logger = require('bunyan');
-var ldap = require('ldapjs');
-var morayClient = require('moray');
 var nopt = require('nopt');
-var retry = require('retry');
-var uuid = require('node-uuid');
-
-
-var be = require('./lib');
-var schema = require('./lib/schema');
 
 // TODO: groups and blacklist
 
-
-
-///--- Globals
+// --- Globals
 
 var LOG = new Logger({
     name: 'ufds',
@@ -48,11 +38,8 @@ var SHORT_OPTS = {
     'h': ['--help']
 };
 
-var SCHEMA;
 
-
-
-///--- Helpers
+// --- Helpers
 
 function usage(code, message) {
     var _opts = '', msg;
@@ -73,12 +60,6 @@ function usage(code, message) {
 
     console.error(msg);
     process.exit(code);
-}
-
-
-function errorAndExit(err, message) {
-    LOG.fatal({err: err}, message);
-    process.exit(1);
 }
 
 
@@ -138,247 +119,14 @@ function processConfig() {
 }
 
 
+// --- Mainline
 
-function audit(req, res, next) {
-    var additional = '', attrs;
-    switch (req.type) {
-    case 'BindRequest':
-        additional += 'bindType=' + req.authentication + ', ';
-        break;
-    case 'AddRequest':
-        attrs = req.toObject().attributes;
-        if (attrs.userpassword) {
-            attrs.userpassword = ['XXXXXX'];
-        }
-        additional += 'entry= ' + JSON.stringify(attrs) + ', ';
-        break;
-    case 'SearchRequest':
-        additional += 'scope=' + req.scope + ', ' +
-            'filter=' + req.filter.toString() + ', ' +
-            'attributes=' + (req.attributes.join() || '[]') + ', ' +
-            'sentEntries=' + res.sentEntries + ', ';
-        break;
-    default:
-        break;
-    }
-
-    LOG.info('clientip=' + (req.connection.remoteAddress || 'localhost') +
-             ', ' +
-             'bindDN=' + req.connection.ldap.bindDN.toString() + ', ' +
-             'msgid=' + req.id + ', ' +
-             'request=' + req.type + ', ' +
-             'requestDN=' + req.dn.toString() + ', ' +
-             additional +
-             'status=' + res.status + ', ' +
-             'time=' + (new Date().getTime() - req.startTime) + 'ms, ');
-}
-
-
-function createMorayClient(options) {
-    assert.ok(options);
-
-    return morayClient.createClient({
-        url: options.moray.url,
-        log: LOG.child({
-            component: 'moray'
-        }),
-        noCache: true,
-        retry: options.moray.retry || false,
-        connectTimeout: options.moray.connectTimeout || 1000
-    });
-}
-
-
-function createServer(options) {
-    assert.ok(options);
-
-    var _server = ldap.createServer(options);
-    _server.after(audit);
-
-    // Admin bind
-    _server.bind(options.rootDN, function (req, res, next) {
-        if (req.version !== 3) {
-            return next(new ldap.ProtocolError(req.version + ' is not v3'));
-        }
-
-        if (req.credentials !== options.rootPassword) {
-            return next(new ldap.InvalidCredentialsError(req.dn.toString()));
-        }
-
-        res.end();
-        return next();
-    });
-
-    // ldapwhoami -H ldap://localhost:1389 -x -D cn=root -w secret
-    // cn=root
-    _server.exop('1.3.6.1.4.1.4203.1.11.3', function (req, res, next) {
-        res.responseValue = req.connection.ldap.bindDN.toString();
-        res.end();
-        return next();
-    });
-
-    // RootDSE
-    _server.search('', function (req, res, next) {
-        function now() {
-            function pad(n) { return ((n < 10) ? '0' + n : n); }
-            var d = new Date();
-            return d.getUTCFullYear() +
-                pad(d.getUTCMonth() + 1) +
-                pad(d.getUTCDate()) +
-                pad(d.getUTCHours()) +
-                pad(d.getUTCMinutes()) +
-                pad(d.getUTCSeconds()) +
-                '.0Z';
-        }
-
-        var suffixes = Object.keys(options.trees);
-        suffixes.push('cn=changelog');
-        var entry = {
-            dn: '',
-            attributes: {
-                namingcontexts: suffixes,
-                supportedcontrol: ['1.3.6.1.4.1.38678.1',
-                    '2.16.840.1.113730.3.4.3'],
-                supportedextension: ['1.3.6.1.4.1.4203.1.11.3'],
-                supportedldapversion: 3,
-                currenttime: now(),
-                objectclass: 'RootDSE'
-            }
-        };
-
-        res.send(entry);
-        res.end();
-        return next();
-    });
-
-    _server.on('clientError', function (err) {
-        LOG.error({err: err}, 'LDAPJS Server clientError');
-    });
-
-    return _server;
-}
-
-
-function listen(_server, config) {
-    return _server.listen(config.port, config.host, function () {
-        LOG.info('UFDS listening at: %s\n\n', _server.url);
-    });
-}
-
-///--- Mainline
-
-function main() {
-    var config = processConfig();
-    var finished = 0;
-    var moray = createMorayClient(config);
-    var server = createServer(config);
-    var trees = config.trees;
-
-    SCHEMA = schema.load(__dirname + '/schema', LOG);
-    LOG.info({schema: Object.keys(SCHEMA)}, 'Schema loaded');
-
-
-
-    moray.once('error', function (err) {
-        errorAndExit(err, 'Moray Error');
-    });
-
-    server.use(function setup(req, res, next) {
-        req.req_id = uuid();
-        req.log = LOG.child({req_id: req.req_id}, true);
-        req.moray = moray;
-        req.schema = SCHEMA;
-        req.config = config;
-
-        return next();
-    });
-
-    function onMorayConnect() {
-        var clog = config.changelog;
-        moray.putBucket(clog.bucket, {index: clog.schema}, function (clogErr) {
-            if (clogErr) {
-                LOG.fatal({err: clogErr}, 'Unable to set changelog bucket');
-                LOG.info('Trying again in 10 seconds');
-                return setTimeout(function () {
-                    onMorayConnect();
-                }, 10000);
-            }
-
-            server.search('cn=changelog',
-                          function _setup(req, res, next) {
-                                req.bucket = clog.bucket;
-                                req.suffix = 'cn=changelog';
-                                return next();
-                          },
-                          be.search());
-
-            return Object.keys(trees).forEach(function (t) {
-                LOG.debug({
-                    bucket: trees[t].bucket,
-                    schema: trees[t].schema,
-                    suffix: t
-                }, 'Configuring UFDS bucket');
-
-                var bucket = trees[t].bucket,
-                    cfg = {
-                        index: trees[t].schema,
-                        post: [
-                            be.changelog.add,
-                            be.changelog.mod,
-                            be.changelog.del
-                        ]
-                    };
-
-                return moray.putBucket(bucket, cfg, function (err) {
-                    if (err) {
-                        LOG.fatal({err: err}, 'Unable to set Moray bucket');
-                        LOG.info('Trying again in 10 seconds');
-                        return setTimeout(function () {
-                            onMorayConnect();
-                        }, 10000);
-                    }
-
-                    function __setup(req, res, next) {
-                        req.bucket = trees[t].bucket;
-                        req.suffix = t;
-
-                        return next();
-                    }
-
-                    server.add(t, __setup, be.add());
-                    server.bind(t, __setup, be.bind());
-                    server.compare(t, __setup, be.compare());
-                    server.del(t, __setup, be.del());
-                    server.modify(t, __setup, be.modify());
-                    server.search(t, __setup, be.search());
-
-                    if (++finished < Object.keys(trees).length)
-                        return false;
-
-                    return listen(server, config);
-                });
-            });
-        });
-    }
-
-    moray.on('connect', onMorayConnect);
-}
-
-var REBOOTING = false;
-process.on('uncaughtException', function (err) {
-    console.log('UncaughtException happened.');
-    LOG.fatal({err: err}, 'uncaughtException');
-    if (REBOOTING !== true) {
-        REBOOTING = true;
-        console.log('Retrying to boot ufds server in 30 seconds.');
-        var t = setTimeout(function () {
-            clearTimeout(t);
-            // Let SMF do its job and restart the server after 30 seconds.
-            // FIXME: Ideally, we'll have a better procedure to boot the server
-            // and cleanup/retry ourselves, w/o any need of exit the process.
-            process.exit(1);
-        }, (30 * 1000));
-    }
+var config = processConfig();
+var ufds = require('./lib/ufds').createServer(config);
+ufds.on('morayError', ufds.morayConnectCalback);
+ufds.init(function () {
+    return (true);
 });
-
-main();
+// Increase/decrease loggers levels using SIGUSR2/SIGUSR1:
+var sigyan = require('sigyan');
+sigyan.signal([LOG, ufds.moray.log]);

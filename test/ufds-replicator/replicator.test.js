467080ee2c97d3159fdb5a2fe9dabc207a7f49ee
Pedro P. Candel <pedro@joyent.com>
2013-09-27 13:29:24 +0200

    CAPI-294: JSLint/JSStyle fixes for replicator files

--- test/ufds-replicator/replicator.test.js
+++ test/ufds-replicator/replicator.test.js
@@ -14,163 +14,164 @@ var rep;
 var bunyan = require('bunyan');
 
 var LOG = bunyan.createLogger({
-	name: 'replicator-test',
-        stream: process.stdout,
-        serializers: bunyan.stdSerializers,
-	level: 'info'
+    name: 'replicator-test',
+    stream: process.stdout,
+    serializers: bunyan.stdSerializers,
+    level: 'info'
 });
 
-var CUSTOMER_DN = 'uuid=930896af-bf8c-48d4-885c-6573a94b1853, ou=users, o=smartdc';
+var CUSTOMER_DN =
+'uuid=930896af-bf8c-48d4-885c-6573a94b1853, ou=users, o=smartdc';
 
 var USERS_QUERY = '/ou=users, o=smartdc??sub?';
 var SERVERS_QUERY = '/ou=servers, datacenter=coal, o=smartdc??sub?';
 var PACKAGES_QUERY = '/ou=packages, o=smartdc??sub?';
 
 var LOCAL_UFDS = {
-	url: process.env.LOCAL_UFDS_URL || 'ldap://127.0.0.1:1389',
-	maxConnections: 1,
-	bindDN: 'cn=root',
-	bindCredentials: 'secret'
+    url: process.env.LOCAL_UFDS_URL || 'ldap://127.0.0.1:1389',
+    maxConnections: 1,
+    bindDN: 'cn=root',
+    bindCredentials: 'secret'
 };
 
 var QUERIES_ONE = [
-	USERS_QUERY,
-	SERVERS_QUERY
+    USERS_QUERY,
+    SERVERS_QUERY
 ];
 
 var QUERIES_TWO = [
-	PACKAGES_QUERY
+    PACKAGES_QUERY
 ];
 
 var REMOTE_ONE = {
-	url: process.env.REMOTE_UFDS_URL || 'ldaps://10.99.99.18',
-	queries: QUERIES_ONE,
-	maxConnections: 1,
-	bindDN: 'cn=root',
-	bindCredentials: 'secret'
+    url: process.env.REMOTE_UFDS_URL || 'ldaps://10.99.99.18',
+    queries: QUERIES_ONE,
+    maxConnections: 1,
+    bindDN: 'cn=root',
+    bindCredentials: 'secret'
 };
 
 var REMOTE_TWO = {
-	url: process.env.REMOTE_UFDS_URL || 'ldaps://10.99.99.18',
-	queries: QUERIES_TWO,
-	maxConnections: 1,
-	bindDN: 'cn=root',
-	bindCredentials: 'secret'
+    url: process.env.REMOTE_UFDS_URL || 'ldaps://10.99.99.18',
+    queries: QUERIES_TWO,
+    maxConnections: 1,
+    bindDN: 'cn=root',
+    bindCredentials: 'secret'
 };
 
 var REPLICATOR_OPTS = {
-	log: LOG,
-	localUfds: LOCAL_UFDS,
-	remotes: [REMOTE_ONE, REMOTE_TWO],
-	checkpointDn: 'cn=replicator, datacenter=coal, o=smartdc'
+    log: LOG,
+    localUfds: LOCAL_UFDS,
+    remotes: [REMOTE_ONE, REMOTE_TWO],
+    checkpointDn: 'cn=replicator, datacenter=coal, o=smartdc'
 };
 
 
-exports.initReplicator = function(t) {
-	rep = new Replicator(REPLICATOR_OPTS);
-	rep.init();
+exports.initReplicator = function (t) {
+    rep = new Replicator(REPLICATOR_OPTS);
+    rep.init();
 
-	rep.once('started', function () {
+    rep.once('started', function () {
         t.done();
-	});
+    });
 };
 
 
 // // Give the replicators time to catch up
-exports.catchUp = function(t) {
-	var done = 0;
+exports.catchUp = function (t) {
+    var done = 0;
 
-	rep.on('caughtup', function (id, cn) {
-		done++;
+    rep.on('caughtup', function (id, cn) {
+        done++;
 
-		if (done == 2) {
-			rep.removeAllListeners('caughtup');
-			t.done();
-		}
-	});
+        if (done == 2) {
+            rep.removeAllListeners('caughtup');
+            t.done();
+        }
+    });
 };
 
 
-exports.addUser = function(t) {
-	var remote = rep.remotes[0].remoteUfds;
+exports.addUser = function (t) {
+    var remote = rep.remotes[0].remoteUfds;
 
-	var user = fixtures.user;
-	var key = fixtures.key;
+    var user = fixtures.user;
+    var key = fixtures.key;
 
-	remote.add(user.dn, user.object, function (errA, resA) {
-		assert.ifError(errA);
+    remote.add(user.dn, user.object, function (errA, resA) {
+        assert.ifError(errA);
 
-		remote.add(key.dn, key.object, function (errB, resB) {
-			assert.ifError(errB);
+        remote.add(key.dn, key.object, function (errB, resB) {
+            assert.ifError(errB);
 
-			t.done();
-		});
-	});
+            t.done();
+        });
+    });
 };
 
 
-exports.catchUpAdd = function(t) {
-	rep.once('caughtup', function (cn) {
-		t.done();
-	});
+exports.catchUpAdd = function (t) {
+    rep.once('caughtup', function (cn) {
+        t.done();
+    });
 };
 
 
-exports.getUser = function(t) {
-	var local = rep.localUfds;
+exports.getUser = function (t) {
+    var local = rep.localUfds;
 
-	var user = fixtures.user;
-	var opts = {
-		scope: 'sub',
-		filter: '(objectclass=*)'
-	};
+    var user = fixtures.user;
+    var opts = {
+        scope: 'sub',
+        filter: '(objectclass=*)'
+    };
 
-	var entries = 0;
+    var entries = 0;
 
-	local.search(user.dn, opts, function (err, res) {
-		assert.ifError(err);
+    local.search(user.dn, opts, function (err, res) {
+        assert.ifError(err);
 
-		res.on('searchEntry', function (entry) {
-			assert.ok(entry);
-			entries++;
-		});
+        res.on('searchEntry', function (entry) {
+            assert.ok(entry);
+            entries++;
+        });
 
-		res.on('end', function (res) {
-			assert.equal(entries, 2);
-			t.done();
-		});
-	});
+        res.on('end', function (res2) {
+            assert.equal(entries, 2);
+            t.done();
+        });
+    });
 };
 
 
-exports.deleteUser = function(t) {
-	var remote = rep.remotes[0].remoteUfds;
+exports.deleteUser = function (t) {
+    var remote = rep.remotes[0].remoteUfds;
 
-	var userDn = fixtures.user.dn;
-	var keyDn = fixtures.key.dn;
+    var userDn = fixtures.user.dn;
+    var keyDn = fixtures.key.dn;
 
-	remote.del(keyDn, function (errA, resA) {
-		assert.ifError(errA);
+    remote.del(keyDn, function (errA, resA) {
+        assert.ifError(errA);
 
-		remote.del(userDn, function (errB, resB) {
-			assert.ifError(errB);
+        remote.del(userDn, function (errB, resB) {
+            assert.ifError(errB);
 
-			t.done();
-		});
-	});
+            t.done();
+        });
+    });
 };
 
 
-exports.catchUpDelete = function(t) {
-	rep.once('caughtup', function (cn) {
-		t.done();
-	});
+exports.catchUpDelete = function (t) {
+    rep.once('caughtup', function (cn) {
+        t.done();
+    });
 };
 
 
-exports.cleanup = function(t) {
-	rep.stop();
-	rep.once('stopped', function () {
+exports.cleanup = function (t) {
+    rep.stop();
+    rep.once('stopped', function () {
         t.done();
     });
 };

a9cdc366260171809be85313c6b9f21b2d370ffb
Pedro P. Candel <pedro@joyent.com>
2013-11-06 13:39:33 +0100

    [CAPI-328] Added memberGroup to sdcAccountRole

--- schema/sdcaccountrole.js
+++ schema/sdcaccountrole.js
@@ -2,11 +2,14 @@
  * Copyright (c) 2013, Joyent, Inc. All rights reserved.
  *
  * This file defines schema for sdcAccountRole, objectclass added to all the
- * groups of a given account. These entries are simmilar to GroupOfUniqueNames
+ * roles of a given account. These entries are simmilar to GroupOfUniqueNames
  * object class.
  *
- * Purpose of this class is to ensure an account group or role has both, an
- * account attribute and one or more policy documents
+ * Purpose of this class is to ensure an account role has both, an
+ * account attribute and one or more policy documents.
+ *
+ * Roles can directly have one or more unique members, or can link account
+ * groups through "memberGroup" attribute.
  */
 
 var assert = require('assert');
@@ -31,6 +34,7 @@ function SDCAccountRole() {
         },
         optional: {
             uniquemember: 1000000,
+            membergroup: 1000000,
             description: 1
         }
     });
@@ -41,11 +45,21 @@ SDCAccountRole.prototype.validate = function validate(entry, config, callback) {
     var attrs = entry.attributes;
     var errors = [];
     var members = attrs.uniquemember || [];
+    var groups = attrs.membergroup || [];
+    var i, j;
 
     members.sort();
-    for (var i = 0; i < members.length; i++) {
+    for (i = 0; i < members.length; i++) {
         if (members.indexOf(members[i], i + 1) !== -1) {
             return callback(new ldap.ConstraintViolationError(members[i] +
+                                                         ' is not unique'));
+        }
+    }
+
+    groups.sort();
+    for (j = 0; j < groups.length; j++) {
+        if (groups.indexOf(groups[j], j + 1) !== -1) {
+            return callback(new ldap.ConstraintViolationError(groups[j] +
                                                          ' is not unique'));
         }
     }
